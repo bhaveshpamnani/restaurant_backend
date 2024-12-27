5d1a94c319e36da764980cb45be65739e41321d1
bhavesh <bhaveshpamnani100@gmail.com>
2024-12-27 18:43:01 +0530

    changes

--- restaurant_backend/obj/Debug/net8.0/restaurant_backend.AssemblyInfo.cs
+++ restaurant_backend/obj/Debug/net8.0/restaurant_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("restaurant_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f58a81d7d99b7ab8317285e5f43c1b19949d73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df69073475e2ded9990c9937916b6bf4f3d244ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("restaurant_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("restaurant_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
