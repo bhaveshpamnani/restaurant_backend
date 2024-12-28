43bc50d7ec0f04c1e3e526ef7fcec8088aead5fa
bhavesh <bhaveshpamnani100@gmail.com>
2024-12-28 19:32:28 +0530

    changes

--- restaurant_backend/Model/DinningTableModel.cs
+++ restaurant_backend/Model/DinningTableModel.cs
@@ -2,7 +2,7 @@ namespace restaurant_backend.Model;
 
 public class DinningTableModel
 {
-    public int TableID { get; set; }
+    public int? TableID { get; set; }
     public string TableCode { get; set; }
     public bool Availability_Status { get; set; }
     public int PersonCount { get; set; }
