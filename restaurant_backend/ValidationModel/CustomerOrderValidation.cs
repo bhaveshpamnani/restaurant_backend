74e0e1a626cd13c3ee51b881b0a7e98db4fe2cd5
bhavesh <bhaveshpamnani100@gmail.com>
2024-12-30 09:04:26 +0530

    "validation done in customer order"

--- restaurant_backend/ValidationModel/CustomerOrderValidation.cs
+++ restaurant_backend/ValidationModel/CustomerOrderValidation.cs
@@ -1,6 +1,20 @@
+using FluentValidation;
+using restaurant_backend.Model;
+
 namespace restaurant_backend.ValidationModel;
 
-public class CustomerOrderValidation
+public class CustomerOrderValidation : AbstractValidator<CustomerOrderModel>
 {
-    
+    public CustomerOrderValidation()
+    {
+        RuleFor(c => c.TableID).NotNull().NotEmpty().WithMessage("Table code is required");
+        RuleFor(c => c.MenuID).NotNull().NotEmpty().WithMessage("At least 1 item is required");
+        RuleFor(c => c.Quantity).NotNull().NotEmpty().WithMessage("Quantity is required")
+            .GreaterThan(0).WithMessage("Quantity must be greater than zero");
+        RuleFor(c => c.TotalAmount).NotNull().NotEmpty().WithMessage("Total amount is required")
+            .GreaterThan(0).WithMessage("Total Amount is greater than 0");
+        RuleFor(c => c.PaymentStatus).NotNull().NotEmpty().WithMessage("Payment status is required");
+        RuleFor(c => c.OrderStatus).NotNull().NotEmpty().WithMessage("OrderStatus  is required");
+    }
+        
 }
